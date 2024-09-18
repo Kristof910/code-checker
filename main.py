668f17c42381e777b492ddfcd4ca1ecb5f28bafe
kristof910 <kristof910@icloud.com>
2024-09-18 19:21:54 +0300

    result page, form fixed

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/", methods=["GET"])
 def index():
     return render_template("index.html")
+
+@app.route("/", methods=["POST"])
+def result():
+    code = request.form["textarea"]
+    print(code)
+    return render_template("result.html", code=code)
 
 if __name__ == '__main__':
     app.run(host='localhost', port=3283)
